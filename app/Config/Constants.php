a33ab0a707cee8cbbd657cd759d75d841cc41422
Deep Suthar <suthardeep2624@gmail.com>
2023-05-13 10:53:24 +0530

    check

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -27,7 +27,7 @@ define('DEVELOPED_BY', 'Deep Suthar');
 
 
 // $base = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']) : 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']);
-$base = "https://deep-ci.netlify.app";
+$base = "https://deep-ci.netlify.app/";
 defined('BASE_URL') || define('BASE_URL', $base);
 define('ASSETS_PATH', BASE_URL.'/assets/');
 define('FILE_PATH', BASE_URL.'/app/views/');
